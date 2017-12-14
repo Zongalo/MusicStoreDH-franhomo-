6d6c1e5b8d18e2d3d18d175bf2425ce8359d56a3
Zongalo <gonzapc1999@gmail.com>
2017-12-14 15:25:59 -0300

    Subir todo final

---
+++ app/Http/Controllers/VientoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Viento;
+use Illuminate\Http\Request;
+
+class VientoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Viento  $viento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Viento $viento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Viento  $viento
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Viento $viento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Viento  $viento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Viento $viento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Viento  $viento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Viento $viento)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/login', function(){
+	return view('login');
+});
+
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
+
+Route::get('/cuerda', function(){
+	return view('categoria-cuerda');
+});
+Route::get('/viento', function(){
+	return view('categoria-viento');
+});
+Route::get('/percucion', function(){
+	return view('categoria-percucion');
+});
+Route::get('/categorias', function(){
+	return view('categorias');
+});
+Route::get('/register', function(){
+	return view('register<');
+});
+
+Route::get('/faq', function(){
+	return view('faq');
+});
+
+Route::get('/perfil', function(){
+	return view('perfil');
+})->middleware('auth');
+
+
+
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
